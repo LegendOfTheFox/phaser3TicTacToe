f71e7fa1798b2420f38480f2773614bb29258eaf
Bryan Fox <bryanfox1986@gmail.com>
2022-10-29 21:24:29 -0400

    mis pushed

--- src/ticTacToe.ts
+++ src/ticTacToe.ts
@@ -1,5 +1,15 @@
 import 'phaser';
 
+enum GameState {
+  PLAYING = 0,
+  END = 1,
+}
+
+interface TilePosition {
+  x: number;
+  y: number;
+}
+
 export default class TicTacToe extends Phaser.Scene {
   boardOffsetX: number;
   boardOffsetY: number;
@@ -8,6 +18,10 @@ export default class TicTacToe extends Phaser.Scene {
   currentPlayerTurn: number;
   gameBoard: number[][];
   boardTileImages: {};
+  score: { player1: number; player2: number };
+  playerScoreImage: Phaser.GameObjects.Text[];
+  gameState: GameState;
+  testParticle: any;
 
   constructor() {
     super('ticTacToe');
@@ -17,6 +31,14 @@ export default class TicTacToe extends Phaser.Scene {
     this.tileWidth = 64;
     this.tileHeight = 64;
     this.currentPlayerTurn = 1;
+    this.score = {
+      player1: 0,
+      player2: 0,
+    };
+
+    this.testParticle = null;
+
+    this.gameState = GameState.PLAYING;
 
     this.updateGameState = this.updateGameState.bind(this);
 
@@ -33,6 +55,14 @@ export default class TicTacToe extends Phaser.Scene {
     ];
   }
 
+  setGameState(state: GameState): void {
+    this.gameState = state;
+  }
+
+  getGameState(): GameState {
+    return this.gameState;
+  }
+
   preload() {
     this.load.image('logo', 'assets/phaser3-logo.png');
     this.load.image('libs', 'assets/libs.png');
@@ -45,19 +75,24 @@ export default class TicTacToe extends Phaser.Scene {
 
     this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
     this.load.glsl('stars', 'assets/starfields.glsl.js');
+
+    this.load.image('fire', 'assets/muzzleflash3.png');
   }
 
-  create() {
+  create(): void {
     this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
 
+    this.testParticle = this.add.particles('fire');
+
     this.drawBoard();
+    this.createScores();
   }
 
-  getCurrentPlayerTurn() {
+  getCurrentPlayerTurn(): number {
     return this.currentPlayerTurn;
   }
 
-  toggleCurrentPlayerTurn() {
+  toggleCurrentPlayerTurn(): void {
     if (this.currentPlayerTurn == 1) {
       this.currentPlayerTurn = 2;
     } else {
@@ -65,10 +100,22 @@ export default class TicTacToe extends Phaser.Scene {
     }
   }
 
+  createScores(): void {
+    this.playerScoreImage = [
+      this.add.text(10, 10, 'Player 1: ' + this.score.player1),
+      this.add.text(10, 30, 'Player 2: ' + this.score.player2),
+    ];
+  }
+
+  drawScores(): void {
+    this.playerScoreImage[0].setText('Player 1: ' + this.score.player1);
+    this.playerScoreImage[1].setText('Player 2: ' + this.score.player2);
+  }
+
   drawBoard(): void {
     for (let i = 1; i < 4; i++) {
       for (let s = 1; s < 4; s++) {
-        let square = this.add
+        const tile = this.add
           .image(
             this.boardOffsetX + this.tileWidth * i,
             this.boardOffsetY + this.tileHeight * s,
@@ -79,34 +126,59 @@ export default class TicTacToe extends Phaser.Scene {
         const row = i - 1;
         const column = s - 1;
 
-        square.setData('boardPosition', { x: row, y: column });
-        square.setData('updateGameState', this.updateGameState);
-        square['name'] = 'test';
-
-        this.boardTileImages[row][column] = square;
-
-        square.on('pointerdown', this.onClick);
+        this.setTileData(tile, { x: row, y: column });
+        this.setTileImage(tile, { x: row, y: column });
+        tile.on('pointerdown', this.onClick);
       }
     }
   }
 
+  setTileData(image, position: TilePosition) {
+    image.setData('boardPosition', position);
+    image.setData('updateGameState', this.updateGameState);
+  }
+
+  setTileImage(image, tilePosition: TilePosition): void {
+    this.boardTileImages[tilePosition.x][tilePosition.y] = image;
+  }
+
   onClick(e: any) {
     let image = this;
-    console.log('image?', image, image['name']);
+    console.log('147 image?', image);
 
-    let tilePosition = image['getData']('boardPosition');
-    let updateGameState = image['getData']('updateGameState');
+    const tilePosition = image['getData']('boardPosition');
+    const updateGameState: Function = image['getData']('updateGameState');
 
-    console.log('tilePosition that was clicked', tilePosition);
+    const test = { x: image['x'], y: image['y'] };
+    //let test;
 
-    updateGameState(tilePosition);
+    updateGameState(tilePosition, test);
   }
 
-  updateTileImage(image) {
-    image['setTexture']('x');
-  }
+  updateGameState(tilePosition: TilePosition, test): void {
+    if (this.getGameState() == GameState.END) {
+      return;
+    }
+    console.log('TEST???');
 
-  updateGameState(tilePosition) {
+    this.testParticle.createEmitter({
+      alpha: { start: 1, end: 0 },
+      scale: { start: 0.5, end: 2.5 },
+      //tint: { start: 0xff945e, end: 0xff945e },
+      speed: 20,
+      accelerationY: -300,
+      angle: { min: -85, max: -95 },
+      rotate: { min: -180, max: 180 },
+      lifespan: { min: 1000, max: 1100 },
+      //blendMode: 'NORMAL',
+      frequency: 50,
+      maxParticles: 10,
+      x: test.x,
+      y: test.y,
+    });
+
+    this.testParticle.setDepth(2);
+
     const tileClicked = this.gameBoard[tilePosition.x][tilePosition.y];
 
     if (!this.checkIfTileIsValid(tileClicked)) {
@@ -115,22 +187,62 @@ export default class TicTacToe extends Phaser.Scene {
     }
 
     const currentPlayer = this.getCurrentPlayerTurn();
-
-    this.gameBoard[tilePosition.x][tilePosition.y] = currentPlayer;
-
-    let image = this.boardTileImages[tilePosition.x][tilePosition.y];
-    image['setTexture'](this.getSquareImage(currentPlayer));
+    this.updateTile(tilePosition, currentPlayer);
 
     const currentPlayerWon = this.checkIfPlayerWon(currentPlayer);
-    console.log('curentPlayerWon?', currentPlayerWon);
+
     if (currentPlayerWon) {
       console.log('Player ' + currentPlayer + ' won!, resetting board');
+      this.updateGamesWon(currentPlayer);
+
+      this.setGameState(GameState.END);
+      setTimeout(() => {
+        this.resetBoard();
+        this.setGameState(GameState.PLAYING);
+      }, 2000);
     } else {
       this.toggleCurrentPlayerTurn();
     }
   }
 
-  checkIfTileIsValid(tileValue) {
+  updateTile(tilePosition, tileValue: number): void {
+    this.updateTileValue(tilePosition, tileValue);
+    this.updateTileImage(tilePosition, tileValue);
+  }
+
+  updateTileValue(tilePosition, tileValue: number): void {
+    this.gameBoard[tilePosition.x][tilePosition.y] = tileValue;
+  }
+
+  updateTileImage(tilePosition, tileValue: number): void {
+    const image = this.boardTileImages[tilePosition.x][tilePosition.y];
+    image['setTexture'](this.getSquareImage(tileValue));
+  }
+
+  updateGamesWon(currentPlayer: number): void {
+    if (currentPlayer == 1) {
+      this.score.player1++;
+    } else {
+      this.score.player2++;
+    }
+  }
+
+  resetBoard(): void {
+    this.gameBoard = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+
+    for (let i = 0; i < 3; i++) {
+      for (let s = 0; s < 3; s++) {
+        let image = this.boardTileImages[i][s];
+        image['setTexture']('blackBorderSquare');
+      }
+    }
+  }
+
+  checkIfTileIsValid(tileValue: number): boolean {
     if (tileValue == 0) {
       return true;
     }
@@ -138,7 +250,7 @@ export default class TicTacToe extends Phaser.Scene {
     return false;
   }
 
-  checkIfPlayerWon(currentPlayer) {
+  checkIfPlayerWon(currentPlayer: number): boolean {
     let result = false;
     // check rows
     for (let i = 0; i < 3; i++) {
@@ -172,10 +284,6 @@ export default class TicTacToe extends Phaser.Scene {
     return result;
   }
 
-  updateBoardPosition() {
-    console.log('gameboard test', this.gameBoard);
-  }
-
   getSquareImage(currentValue: number) {
     if (currentValue == 2) {
       return 'o';
@@ -186,7 +294,7 @@ export default class TicTacToe extends Phaser.Scene {
     }
   }
 
-  update() {}
-
-  clearBoard() {}
+  update(time: number, delta: number): void {
+    this.drawScores();
+  }
 }
